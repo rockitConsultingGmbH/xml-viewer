9d8d0eeee8a383a5a597576d52bdb0227acb3073
Viacheslav Rashchevskyi <viacheslav.rashchevskyi@rockit.consulting>
2024-10-17 13:26:17 +0200

    Re-worked save function and populating for Description table

--- gui/components/descriptions.py
+++ gui/components/descriptions.py
@@ -3,8 +3,9 @@ from controllers.utils.get_db_connection import get_db_connection
 from database.utils import select_from_description
 from utils.clickable_label import ClickableLabel
 
-def create_description_form(label_style, communication_id):
+input_names = []
 
+def create_description_form(label_style, communication_id):
     form_layout_right = QFormLayout()
     description_label = ClickableLabel("Description(s)")
     description_label.setStyleSheet(label_style)
@@ -15,10 +16,12 @@ def create_description_form(label_style, communication_id):
     for description in descriptions:
         description_label = QLabel(description["descriptionType"])
         description_input = QLineEdit(description["description"])
-        description_input.setObjectName(f"description_[{description['id']}]_input")
+        object_name = f"description_{description['id']}_input"
+        description_input.setObjectName(object_name)
         description_input.setFixedSize(550, 30)
 
         form_layout_right.addRow(description_label, description_input)
+        input_names.append(object_name)
 
     conn.close()
 
